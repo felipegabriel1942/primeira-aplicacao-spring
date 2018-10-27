b6fa0fbf1a03c412ba8f333225ddbb5788deb72b
felipegabriel1942 <pinheiro_felipeg@yahoo.com.br>
2018-10-27 17:13:11 -0300

    Projeto criado

---
+++ src/main/java/com/felipe/spring1/AplicativoSpring1Application.java
@@ -0,0 +1,12 @@
+package com.felipe.spring1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplicativoSpring1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplicativoSpring1Application.class, args);
+	}
+}
