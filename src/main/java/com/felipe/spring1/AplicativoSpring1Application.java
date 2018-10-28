397fe001d4076afa19eec6a777425ade9df5b900
felipegabriel1942 <pinheiro_felipeg@yahoo.com.br>
2018-10-28 16:31:33 -0300

    Criando operação de inserção

--- src/main/java/com/felipe/spring1/AplicativoSpring1Application.java
+++ src/main/java/com/felipe/spring1/AplicativoSpring1Application.java
@@ -1,12 +1,30 @@
 package com.felipe.spring1;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class AplicativoSpring1Application {
+import com.felipe.spring1.domain.Categoria;
+import com.felipe.spring1.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class AplicativoSpring1Application implements CommandLineRunner{
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(AplicativoSpring1Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null,"Informática");
+		Categoria cat2 = new Categoria(null,"Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
 	}
 }
