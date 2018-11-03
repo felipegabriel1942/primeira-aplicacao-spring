aa1cf8527a764f4074ad51e114ac83ddc0c06cbd
felipegabriel1942 <pinheiro_felipeg@yahoo.com.br>
2018-11-03 19:09:46 -0300

    Deletando uma categoria

--- src/main/java/com/felipe/spring1/services/CategoriaService.java
+++ src/main/java/com/felipe/spring1/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.felipe.spring1.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.felipe.spring1.domain.Categoria;
 import com.felipe.spring1.repositories.CategoriaRepository;
+import com.felipe.spring1.services.exceptions.DataIntegrityException;
 import com.felipe.spring1.services.exceptions.ObjectNotFoundException;
 
 
@@ -32,5 +34,15 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+	
+	public void delete (Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possivel excluir uma categoria que possui produtos");
+		}
+		
 	}
 }
