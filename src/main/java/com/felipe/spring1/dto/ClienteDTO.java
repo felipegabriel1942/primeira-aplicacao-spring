43f7ce3cd2fb48e7023b4c5ed78a129eac23f3e1
felipegabriel1942 <pinheiro_felipeg@yahoo.com.br>
2018-11-05 19:55:29 -0300

    Validação customizada: email não repetido na atualização do cliente

--- src/main/java/com/felipe/spring1/dto/ClienteDTO.java
+++ src/main/java/com/felipe/spring1/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.felipe.spring1.domain.Cliente;
+import com.felipe.spring1.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable{
 
 	private static final long serialVersionUID = 1L;
