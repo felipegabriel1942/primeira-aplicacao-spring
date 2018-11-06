f468b8d71389c6f5ff51ef6d3c46df751983fda7
felipegabriel1942 <pinheiro_felipeg@yahoo.com.br>
2018-11-05 21:59:14 -0300

    Busca de produtos por nome e categorias

---
+++ src/main/java/com/felipe/spring1/dto/ProdutoDTO.java
@@ -0,0 +1,49 @@
+package com.felipe.spring1.dto;
+
+import java.io.Serializable;
+
+import com.felipe.spring1.domain.Produto;
+
+public class ProdutoDTO implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private Integer id;
+	private String nome;
+	private Double preço;
+
+	public ProdutoDTO() {
+
+	}
+
+	public ProdutoDTO(Produto obj) {
+		id = obj.getId();
+		nome = obj.getNome();
+		preço = obj.getPreço();
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Double getPreço() {
+		return preço;
+	}
+
+	public void setPreço(Double preço) {
+		this.preço = preço;
+	}
+
+}
